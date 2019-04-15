db2823a59ba59298cae7121cbf500e17a1a76f12
Adrian Estrada <edsadr@gmail.com>
2019-04-15 10:29:15 -0500

    02 - Queries y Resolvers

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const { graphql, buildSchema } = require('graphql')
 const schema = buildSchema(`
   type Query {
     hello: String
+    saludo: String
   }
 `)
 
+// Configurar los resolvers
+const resolvers = {
+  hello: () => {
+    return 'Hola Mundo'
+  },
+  saludo: () => {
+    return 'Hola a todos'
+  }
+}
+
 // Ejecutar el query hello
-graphql(schema, '{ hello }').then((data) => {
+graphql(schema, '{ saludo }', resolvers).then((data) => {
   console.log(data)
 })
