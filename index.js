137b9d1008291dcad11071100a4db6705863905c
Adrian Estrada <edsadr@gmail.com>
2019-04-15 10:49:18 -0500

    03 - Sirviendo el API en la web

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict'
 
 const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+
+const app = express()
+const port = process.env.port || 3000
 
 // definiendo el esquema
 const schema = buildSchema(`
   type Query {
+    "Retorna un saludo al mundo"
     hello: String
-    saludo: String
   }
 `)
 
@@ -14,13 +19,15 @@ const schema = buildSchema(`
 const resolvers = {
   hello: () => {
     return 'Hola Mundo'
-  },
-  saludo: () => {
-    return 'Hola a todos'
   }
 }
 
-// Ejecutar el query hello
-graphql(schema, '{ saludo }', resolvers).then((data) => {
-  console.log(data)
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
 })
